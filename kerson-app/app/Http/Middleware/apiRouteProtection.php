e2afe81506b588ec86c65622b70f39cb94972d7b
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2020-10-02 00:48:13 -0300

    Criada a Controller apiRouteProtection e as suas configurações

--- kerson-app/app/Http/Middleware/apiRouteProtection.php
+++ kerson-app/app/Http/Middleware/apiRouteProtection.php
@@ -3,9 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
-class apiRouteProtection
+class apiRouteProtection extends BaseMiddleware
 {
     /**
      * Handle an incoming request.
@@ -16,6 +19,28 @@ class apiRouteProtection
      */
     public function handle(Request $request, Closure $next)
     {
+        try {
+
+
+            $user = JWTAuth::parseToken()->authenticate();
+
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status' => 'Token is Invalid']);
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status' => 'Token is Expired']);
+            }else {
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+
+
+
+        }
         return $next($request);
+
     }
+
+
 }
+
+
